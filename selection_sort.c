e600bd268592ad373a81b0f775b638d805e564d0
Naga Saranya tulasi haridasu <saranyatulasiharidasu@gmail.com>
2025-03-02 20:03:22 +0530

    Add files via upload

---
+++ selection_sort.c
@@ -0,0 +1,30 @@
+ #include<stdio.h>
+int main()
+{
+int n,j;
+scanf("%d",&n);
+int a[n];
+for(int i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(int i=0;i<n-1;i++)
+{
+	int min=i;
+	for(int j=i+1;j<n;j++)
+	{
+		if(a[j]<a[min])
+		min=j;
+	}
+	if(min != i)
+	{
+		int temp=a[min];
+		a[min]=a[i];
+		a[i]=temp;
+	}
+}
+for(int i=0;i<n;i++)
+{
+	printf("%d ",a[i]); 
+}
+}
